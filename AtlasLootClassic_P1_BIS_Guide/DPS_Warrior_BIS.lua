5ed393654d74549e38e8070c2e25f23b5f92f50b
OrionShock <Orionshock@gmail.com>
2021-08-23 14:28:48 -0700

    Update DPS_Warrior_BIS.lua

--- AtlasLootClassic_P1_BIS_Guide/DPS_Warrior_BIS.lua
+++ AtlasLootClassic_P1_BIS_Guide/DPS_Warrior_BIS.lua
@@ -85,118 +85,210 @@ data[ _FilePrefix ] ={
 	gameVersion = 2,
 	items = {
 		{
-			name = "Set1",
+			name = "BiS Example for Arms",
 			[NORMAL_DIFF] = {
-
+				{ 01, 29021 },	{ 16, 28779 },
+				{ 02, 29349 },	{ 17, 28741 },
+				{ 03, 29023 },	{ 18, 28608 },
+				{ 04, 24259 },	{ 19, 28757 },
+				{ 05, 29019 },	{ 20, 28730 },
+				{ 06, 28795 },	{ 21, 29383 },
+				{ 07, 28824 },	{ 22, 28034 },
+				{ 09, 28429 },
+				{ 11, 30279 },
+			}
+		},
+		{
+			name = "BiS Example for Fury",
+			[NORMAL_DIFF] = {
+				{ 01, 29021 },	{ 16, 28779 },
+				{ 02, 29381 },	{ 17, 28741 },
+				{ 03, 29023 },	{ 18, 28608 },
+				{ 04, 24259 },	{ 19, 28757 },
+				{ 05, 29019 },	{ 20, 30834 },
+				{ 06, 28795 },	{ 21, 29383 },
+				{ 07, 28824 },	{ 22, 28830 },
+				{ 09, 28438 },
+				{ 10, 28729 },
+				{ 11, 30279 },
 			}
 		},
 		{	--Head Slot
 			name = "Head",
 			[NORMAL_DIFF] = {
-
+				{1,29021, nil, nil, "Best" },
+				{2,32087, nil, nil, "Pre-raid Best" },
+				{3,28182, nil, nil, "Easy To Obtain" },
+				{4,31105, nil, nil, "Easy To Obtain" },
 			}
 		},
 		{	--Shoulders
 			name = "Shoulders",
 			[NORMAL_DIFF] = {
-
+				{1,29023, nil, nil, "Best" },
+				{2,30740, nil, nil, "World Boss Best" },
+				{3,33173, nil, nil, "Crafted Best" },
+				{4,27797, nil, nil, "Optional" },
+				{5,27434, nil, nil, "Optional" },
 			},
 		},
 		{	--Back
 			name = "Back",
 			[NORMAL_DIFF] = {
-
+				{1,24259, nil, nil, "Best" },
+				{2,30729, nil, nil, "World Boss Best" },
+				{3,28672, nil, nil, "Karazhan Optional" },
+				{4,27878, nil, nil, "Optional" },
+				{5,27892, nil, nil, "Easy To Obtain" },
 			},
 		},
 		{	--Chest
 			name = "Chest",
 			[NORMAL_DIFF] = {
-
+				{1,29019, nil, nil, "Best" },
+				{2,30730, nil, nil, "World Boss Best" },
+				{3,23522, nil, nil, "Crafted Option" },
+				{4,24544, nil, nil, "Optional" },
+				{5,31548, nil, nil, "Easy To Obtain" },
 			},
 		},
 		{	--Wrist
 			name = "Wrist",
 			[NORMAL_DIFF] = {
-
+				{1,28795, nil, nil, "Best" },
+				{2,23537, nil, nil, "Optional" },
+				{3,28381, nil, nil, "Optional" },
+				{4,28171, nil, nil, "Easy To Obtain" },
+				{5,30399, nil, nil, "Easy To Obtain" },
 			},
 
 		},
 		{	--Hands
 			name = "Hands",
 			[NORMAL_DIFF] = {
-
+				{1,28824, nil, nil, "Best" },
+				{2,30644, nil, nil, "Karazhan Option" },
+				{3,29020, nil, nil, "Karazhan Option" },
+				{4,25685, nil, nil, "Crafted Best" },
+				{5,27497, nil, nil, "Dungeon Optional" },
 			},
 		},
 		{	--Waist
 			name = "Waist",
 			[NORMAL_DIFF] = {
-
+				{1,28779, nil, nil, "Best" },
+				{2,28828, nil, nil, "Raid Option" },
+				{3,27985, nil, nil, "Dungeon Optional" },
+				{4,29247, nil, nil, "Optional" },
 			},
 		},
 		{	--Legs
 			name = "Legs",
 			[NORMAL_DIFF] = {
-
+				{1,30739, nil, nil, "Best" },
+				{2,28741, nil, nil, "Raid Best" },
+				{3,29022, nil, nil, "Raid Alternative" },
+				{4,30538, nil, nil, "Dungeon Best" },
+				{5,25687, nil, nil, "Crafted Optional" },
+				{6,31544, nil, nil, "Easy to Obtain" },
 			},
 		},
 		{	--Feet
 			name = "Feet",
 			[NORMAL_DIFF] = {
-
+				{1,28608, nil, nil, "Best" },
+				{2,25686, nil, nil, "Crafted Best" },
+				{3,28545, nil, nil, "Optional" },
+				{4,27867, nil, nil, "Optional" },
+				{5,30401, nil, nil, "Easy To Obtain" },
 			},
 		},
 		{	--Neck
 			name = "Neck",
 			[NORMAL_DIFF] = {
-
+				{1,29381, nil, nil, "Best" },
+				{2,29349, nil, nil, "Arms Best" },
+				{3,28509, nil, nil, "Optional" },
+				{4,31695, nil, nil, "Easy To Obtain" },
+				{5,27546, nil, nil, "Optional" },
 			},
 		},
 		{	--Rings
 			name = "Rings",
 			[NORMAL_DIFF] = {
-
+				{1,30738, nil, nil, "World Boss Best" },
+				{2,28757, nil, nil, "Best" },
+				{3,28730, nil, nil, "Arms Best" },
+				{4,30834, nil, nil, "Reputation Option" },
+				{5,28649, nil, nil, "Raid Option" },
+				{6,29283, nil, nil, "Reputation Option" },
+				{7,29379, nil, nil, "Optional" },
+				{8,28323, nil, nil, "Optional" },
 			},
 		},
-		{	--Trinkets 1
-			name = "Trinkets1",
+		{	--Trinkets
+			name = "Trinkets",
 			[NORMAL_DIFF] = {
-
+				{1,28830, nil, nil, "Best" },
+				{2,29383, nil, nil, "Best" },
+				{3,28034, nil, nil, "Optional" },
+				{4,28288, nil, nil, "Optional" },
+				{5,21670, nil, nil, "Classic Option" },
 			},
 		},
-		{	--Trinkets 2
-			name = "Trinkets2",
+		{	--Main Hand Weapons
+			name = "Main Hand Weapons",
 			[NORMAL_DIFF] = {
-
+				{1,28438, nil, nil, "Best" },
+				{2,28437, nil, nil, "Easier to Obtain" },
+				{3,28295, nil, nil, "Optional" },
+				{4,28657, nil, nil, "Human Racial" },
+				{5,28767, nil, nil, "Orc Racial" },
+				{6,23054, nil, nil, "Classic" },
 			},
 		},
-		{	--Trinkets 3
-			name = "Trinkets3",
+		{	--Off Hand Weapons
+			name = "Off Hand Weapons",
 			[NORMAL_DIFF] = {
-
+				{1,28295, nil, nil, "Best" },
+				{2,28729, nil, nil, "Best" },
+				{3,30733, nil, nil, "World Boss Best" },
+				{4,23542, nil, nil, "Orc Racial" },
+				{5,29124, nil, nil, "Optional" },
 			},
 		},
-		{	--1H Wep
-			name = "1-Handed Weapons",
+		{	--2-Handed Weapon
+			name = "2-Handed Weapon",
 			[NORMAL_DIFF] = {
-
+				{1,28429, nil, nil, "Best" },
+				{2,28573, nil, nil, "Optional" },
+				{3,28773, nil, nil, "Orc Optional" },
+				{4,28441, nil, nil, "Hammersmith" },
+				{5,24550, nil, nil, "Optional" },
+				{6,28428, nil, nil, "Easier to Acquire" },
 			},
 		},
-		{	--2H Wep
-			name = "2-Handed Weapons",
+		{	--Guns and Bows
+			name = "Guns and Bows",
 			[NORMAL_DIFF] = {
-
+				{1,30724, nil, nil, "Best" },
+				{2,28772, nil, nil, "Best" },
+				{3,30279, nil, nil, "Best" },
+				{4,29211, nil, nil, "Easy To Obtain" },
+				{5,22811, nil, nil, "Classic Optional" },
 			},
 		},
-		{	--Ranged
-			name = "Ranged",
+		{	--Shields
+			name = "Shields",
 			[NORMAL_DIFF] = {
-
-			},
-		},
-		{	--Offhand
-			name = "Off Hand",
-			[NORMAL_DIFF] = {
-
+				{1,28825, nil, nil, "Best" },
+				{2,28358, nil, nil, "Best" },
+				{3,28606, nil, nil, "Best" },
+				{4,29266, nil, nil, "Optional" },
+				{5,29176, nil, nil, "Optional" },
+				{6,28316, nil, nil, "Easy To Obtain" },
+				{7,27887, nil, nil, "Easy To Obtain" },
+				{8,23043, nil, nil, "Classic Optional" },
 			},
 		},
 		el_Gems,
