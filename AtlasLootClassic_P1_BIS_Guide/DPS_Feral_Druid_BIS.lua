2d1ba4beb40a6862f718a9d59a93096404ccf8ca
OrionShock <Orionshock@gmail.com>
2021-08-24 23:52:39 -0700

    Add Feral/Cat Druid Pre/BIS Set

--- AtlasLootClassic_P1_BIS_Guide/DPS_Feral_Druid_BIS.lua
+++ AtlasLootClassic_P1_BIS_Guide/DPS_Feral_Druid_BIS.lua
@@ -21,11 +21,11 @@ local el_Gems = {	--Extra List : Gems
 	ExtraList = true,
 	TableType = PROF_ITTYPE,
 	[NORMAL_DIFF] = {
-        { 01, "INV_MISC_DESECRATED_LEATHERHELM", nil, "Meta Gem" },
-
-        { 04, "INV_MISC_GEM_01", nil, "Primary"},
-
-        { 07, "INV_MISC_GEM_03", nil, "Meta Fit"},
+		{ 01, "INV_MISC_GEM_01", nil, "Primary"},
+		{ 02, 31085 },
+		
+		{ 04, "INV_MISC_GEM_03", nil, "Meta Fit"},
+		{ 05, 31109 },
 	}
 }
 
@@ -35,9 +35,10 @@ local el_HeadShoulder = {	--Extra List: Shoulder Items
 	TableType = NORMAL_ITTYPE,
 	[NORMAL_DIFF] = {
 		{ 01, "INV_MISC_DESECRATED_LEATHERHELM", nil, "Head Glyph"},
-
+		{ 02, 29192 },
 		{ 04, "INV_MISC_DESECRATED_LEATHERSHOULDER", nil, "Shoulder Inscription"},
-
+		{ 05, 23548 },
+		{ 06, 28888 },	{ 21, 28910 },
 	}
 }
 
@@ -47,17 +48,14 @@ local el_GearEnchants = {	--Extra List: Gear Enchants
 	TableType = PROF_ITTYPE,
 	[NORMAL_DIFF] = {
 		{ 01 , "INV_MISC_CAPE_20", nil, "Back" },	
-		
-		{ 16, "INV_MISC_DESECRATED_LEATHERCHEST", nil, "Chest" },
-
-		{ 04 , "INV_MISC_DESECRATED_LEATHERBRACER", nil, "Wrist" },
-
-		{ 08 , "INV_MISC_DESECRATED_LEATHERGLOVE", nil, "Hands" },
-
-		{ 11 , "INV_MISC_DESECRATED_LEATHERPANTS", nil, "Legs" },
-
-		{ 14 , "INV_MISC_DESECRATED_LEATHERBOOTS", nil, "Feet" },
-
+		{ 02, 46505 },	
+		{ 04, "INV_MISC_DESECRATED_LEATHERCHEST", nil, "Chest" },	{ 19 , "INV_MISC_DESECRATED_LEATHERBRACER", nil, "Wrist" },
+		{ 05, 27960 },	{ 20, 27899 },
+		{ 07 , "INV_MISC_DESECRATED_LEATHERPANTS", nil, "Legs" },	{ 22 , "INV_MISC_DESECRATED_LEATHERGLOVE", nil, "Hands" },
+		{ 08, 35549 },	{ 23, 25080 },
+		{ 09, 35554 },	
+		{ 11 , "INV_MISC_DESECRATED_LEATHERBOOTS", nil, "Feet" },	
+		{ 12, 34007 },	
 	},
 }
 
@@ -66,11 +64,10 @@ local el_offEnchants = {	--Extra List: Ring/Wep/Sheild Enchants
 	ExtraList = true,
 	TableType = PROF_ITTYPE,
 	[NORMAL_DIFF] = {
-		{ 1, "INV_JEWELRY_RING_23", nil, "Ring Enchant" },
-
-		{ 4, "INV_SWORD_65", nil, "Weapon Enchant"},
-
-		{ 7, "INV_SHIELD_30", nil, "Shield Enchant" }, 
+		{ 01, "INV_JEWELRY_RING_23", nil, "Ring Enchant" },
+		{ 02, 27920 },
+		{ 04, "INV_SWORD_65", nil, "Weapon Enchant"},
+		{ 05, 27977 },
 
 	},
 }
@@ -83,118 +80,177 @@ data[ _FilePrefix ] ={
 	gameVersion = 2,
 	items = {
 		{
-			name = "Set1",
+			name = "BIS with out Faerie Fire",
 			[NORMAL_DIFF] = {
-
+				{ 01, 8345 },	{ 16, 28750 },
+				{ 02, 24114 },	{ 17, 28741 },
+				{ 03, 29100 },	{ 18, 28545 },
+				{ 04, 28672 },	{ 19, 28649 },
+				{ 05, 29096 },	{ 20, 30834 },
+				{ 06, 29246 },	{ 21, 28830 },
+				{ 07, 28506 },	{ 22, 29383 },
+				{ 09, 28658 },
+				{ 11, 29390 },
+			}
+		},
+		{
+			name = "BIS with Faerie Fire",
+			[NORMAL_DIFF] = {
+				{ 01, 8345 },	{ 16, 28750 },
+				{ 02, 24114 },	{ 17, 28741 },
+				{ 03, 29100 },	{ 18, 28545 },
+				{ 04, 24259 },	{ 19, 28791 },
+				{ 05, 29096 },	{ 20, 30834 },
+				{ 06, 29246 },	{ 21, 28830 },
+				{ 07, 28506 },	{ 22, 29383 },
+				{ 09, 28476 },
+				{ 11, 29390 },
 			}
 		},
 		{	--Head Slot
 			name = "Head",
 			[NORMAL_DIFF] = {
-
+				{1, 8345, nil, nil, "Still BIS"}
 			}
 		},
 		{	--Shoulders
 			name = "Shoulders",
 			[NORMAL_DIFF] = {
-
+				{1,29100, nil, nil, "BiS" },
+				{2,27797, nil, nil, "Close Second" },
+				{3,25790, nil, nil, "Alliance Alternative" },
+				{4,28755, nil, nil, "Alternative" },
+				{5,27434, nil, nil, "Alternative" },
 			},
 		},
 		{	--Back
 			name = "Back",
 			[NORMAL_DIFF] = {
-
+				{1,28672, nil, nil, "BiS" },
+				{2,24259, nil, nil, "Best in 5% hit set" },
+				{3,31255, nil, nil, "Alternative" },
+				{4,27878, nil, nil, "Alternative" },
+				{5,29382, nil, nil, "Alternative in 5% hit set" },
 			},
 		},
 		{	--Chest
 			name = "Chest",
 			[NORMAL_DIFF] = {
-
+				{1,29096, nil, nil, "BiS" },
+				{2,30730, nil, nil, "Alternative" },
+				{3,28601, nil, nil, "Alternative" },
+				{4,29525, nil, nil, "Alternative" },
+				{5,24396, nil, nil, "Alternative" },
 			},
 		},
 		{	--Wrist
 			name = "Wrist",
 			[NORMAL_DIFF] = {
-
+				{1,29246, nil, nil, "Recommended" },
+				{2,30685, nil, nil, "of the Tiger, Best (unrealistic)" },
+				{3,30685, nil, nil, "of Agility, Alternative (unrealistic)" },
+				{4,28171, nil, nil, "Recommended Alternative" },
+				{5,21602, nil, nil, "Recommended Alternative" },
 			},
 
 		},
 		{	--Hands
 			name = "Hands",
 			[NORMAL_DIFF] = {
-
+				{1,28506, nil, nil, "BiS" },
+				{2,30644, nil, nil, "Close Second" },
+				{3,29507, nil, nil, "Alternative" },
+				{4,25685, nil, nil, "Alternative" },
+				{5,27509, nil, nil, "Recommended Pre-raid" },
 			},
 		},
 		{	--Waist
 			name = "Waist",
 			[NORMAL_DIFF] = {
-
+				{1,29247, nil, nil, "Best Individually" },
+				{2,30676, nil, nil, "of the Tiger, Best in 9% hit set (unrealistic)" },
+				{3,28750, nil, nil, "Recommended in 9% hit set" },
+				{4,28828, nil, nil, "Recommended in 9% hit set" },
+				{5,30372, nil, nil, "Scryer Alternative" },
+				{6,27911, nil, nil, "Alternative" },
+				{7,31464, nil, nil, "Alternative" },
+				{8,27760, nil, nil, "Alternative" },
 			},
 		},
 		{	--Legs
 			name = "Legs",
 			[NORMAL_DIFF] = {
-
+				{1,28741, nil, nil, "BiS" },
+				{2,31544, nil, nil, "Alternative" },
+				{3,27837, nil, nil, "Alternative" },
+				{4,25687, nil, nil, "Alternative" },
+				{5,30538, nil, nil, "Alternative" },
 			},
 		},
 		{	--Feet
 			name = "Feet",
 			[NORMAL_DIFF] = {
-
+				{1,28545, nil, nil, "BiS" },
+				{2,25686, nil, nil, "Alternative" },
+				{3,31288, nil, nil, "Alternative" },
+				{4,29248, nil, nil, "Alternative" },
+				{5,30939, nil, nil, "Alternative" },
 			},
 		},
 		{	--Neck
 			name = "Neck",
 			[NORMAL_DIFF] = {
-
+				{1,24114, nil, nil, "BiS" },
+				{2,29381, nil, nil, "Best budget option" },
+				{3,29119, nil, nil, "Best budget option in 6% hit set" },
+				{4,28509, nil, nil, "Alternative" },
+				{5,31275, nil, nil, "Alternative" },
+				{6,27779, nil, nil, "Alternative" },
+				{7,28745, nil, nil, "Alternative in 6% hit set" },
 			},
 		},
 		{	--Rings
 			name = "Rings",
 			[NORMAL_DIFF] = {
-
+				{1,30834, nil, nil, "BiS" },
+				{2,28649, nil, nil, "Best in 9% hit set" },
+				{3,28791, nil, nil, "Best in 6% hit set" },
+				{4,28757, nil, nil, "Alternative" },
+				{5,30738, nil, nil, "Alternative" },
+				{6,30365, nil, nil, "Aldor Alternative" },
+				{7,29283, nil, nil, "Alternative" },
+				{8,27925, nil, nil, "Alternative" },
 			},
 		},
-		{	--Trinkets 1
-			name = "Trinkets1",
+		{	--Trinkets
+			name = "Trinkets",
 			[NORMAL_DIFF] = {
-
+				{1,23207, nil, nil, "BiS" },
+				{2,28830, nil, nil, "BiS" },
+				{3,29383, nil, nil, "BiS" },
+				{4,23041, nil, nil, "Alternative" },
+				{5,28034, nil, nil, "Alternative" },
+				{6,19406, nil, nil, "Alternative" },
+				{7,28121, nil, nil, "Alternative" },
 			},
 		},
-		{	--Trinkets 2
-			name = "Trinkets2",
+		{	--Weapons
+			name = "Weapons",
 			[NORMAL_DIFF] = {
-
+				{1,28658, nil, nil, "BiS" },
+				{2,28476, nil, nil, "Best in 6% hit set" },
+				{3,29359, nil, nil, "Alternative" },
+				{4,31334, nil, nil, "Alternative" },
+				{5,29171, nil, nil, "Alternative" },
+				{6,27877, nil, nil, "Alternative" },
 			},
 		},
-		{	--Trinkets 3
-			name = "Trinkets3",
+		{	--Idols
+			name = "Idols",
 			[NORMAL_DIFF] = {
-
-			},
-		},
-		{	--1H Wep
-			name = "1-Handed Weapons",
-			[NORMAL_DIFF] = {
-
-			},
-		},
-		{	--2H Wep
-			name = "2-Handed Weapons",
-			[NORMAL_DIFF] = {
-
-			},
-		},
-		{	--Ranged
-			name = "Ranged",
-			[NORMAL_DIFF] = {
-
-			},
-		},
-		{	--Offhand
-			name = "OffHand",
-			[NORMAL_DIFF] = {
-
+				{1,29390, nil, nil, "BiS" },
+				{2,28372, nil, nil, "Recommended Pre-raid" },
+				{3,28064, nil, nil, "Pre-raid Alternative" },
 			},
 		},
 		el_Gems,
--- AtlasLootClassic_P1_PreBIS_Guide/DPS_Feral_Druid_PreBIS.lua
+++ AtlasLootClassic_P1_PreBIS_Guide/DPS_Feral_Druid_PreBIS.lua
@@ -21,11 +21,11 @@ local el_Gems = {	--Extra List : Gems
 	ExtraList = true,
 	TableType = PROF_ITTYPE,
 	[NORMAL_DIFF] = {
-        { 01, "INV_MISC_DESECRATED_LEATHERHELM", nil, "Meta Gem" },
-
-        { 04, "INV_MISC_GEM_01", nil, "Primary"},
-
-        { 07, "INV_MISC_GEM_03", nil, "Meta Fit"},
+		{ 01, "INV_MISC_GEM_01", nil, "Primary"},
+		{ 02, 31085 },
+		
+		{ 04, "INV_MISC_GEM_03", nil, "Meta Fit"},
+		{ 05, 31109 },
 	}
 }
 
@@ -35,9 +35,10 @@ local el_HeadShoulder = {	--Extra List: Shoulder Items
 	TableType = NORMAL_ITTYPE,
 	[NORMAL_DIFF] = {
 		{ 01, "INV_MISC_DESECRATED_LEATHERHELM", nil, "Head Glyph"},
-
+		{ 02, 29192 },
 		{ 04, "INV_MISC_DESECRATED_LEATHERSHOULDER", nil, "Shoulder Inscription"},
-
+		{ 05, 28888 },	{ 20, 28910 },
+		{ 06, 20077 },
 	}
 }
 
@@ -47,17 +48,14 @@ local el_GearEnchants = {	--Extra List: Gear Enchants
 	TableType = PROF_ITTYPE,
 	[NORMAL_DIFF] = {
 		{ 01 , "INV_MISC_CAPE_20", nil, "Back" },	
-		
-		{ 16, "INV_MISC_DESECRATED_LEATHERCHEST", nil, "Chest" },
-
-		{ 04 , "INV_MISC_DESECRATED_LEATHERBRACER", nil, "Wrist" },
-
-		{ 08 , "INV_MISC_DESECRATED_LEATHERGLOVE", nil, "Hands" },
-
-		{ 11 , "INV_MISC_DESECRATED_LEATHERPANTS", nil, "Legs" },
-
-		{ 14 , "INV_MISC_DESECRATED_LEATHERBOOTS", nil, "Feet" },
-
+		{ 02, 46505 },	
+		{ 04, "INV_MISC_DESECRATED_LEATHERCHEST", nil, "Chest" },	{ 19 , "INV_MISC_DESECRATED_LEATHERBRACER", nil, "Wrist" },
+		{ 05, 27960 },	{ 20, 27899 },
+		{ 07 , "INV_MISC_DESECRATED_LEATHERPANTS", nil, "Legs" },	{ 22 , "INV_MISC_DESECRATED_LEATHERGLOVE", nil, "Hands" },
+		{ 08, 35549 },	{ 23, 25080 },
+		{ 09, 35554 },	
+		{ 11 , "INV_MISC_DESECRATED_LEATHERBOOTS", nil, "Feet" },	
+		{ 12, 34007 },	
 	},
 }
 
@@ -66,11 +64,10 @@ local el_offEnchants = {	--Extra List: Ring/Wep/Sheild Enchants
 	ExtraList = true,
 	TableType = PROF_ITTYPE,
 	[NORMAL_DIFF] = {
-		{ 1, "INV_JEWELRY_RING_23", nil, "Ring Enchant" },
-
-		{ 4, "INV_SWORD_65", nil, "Weapon Enchant"},
-
-		{ 7, "INV_SHIELD_30", nil, "Shield Enchant" }, 
+		{ 01, "INV_JEWELRY_RING_23", nil, "Ring Enchant" },
+		{ 02, 27920 },
+		{ 04, "INV_SWORD_65", nil, "Weapon Enchant"},
+		{ 05, 27977 },
 
 	},
 }
@@ -83,118 +80,163 @@ data[ _FilePrefix ] ={
 	gameVersion = 2,
 	items = {
 		{
-			name = "Set1",
+			name = "Karazhan - Pre Raid BIS",
 			[NORMAL_DIFF] = {
-
+				{ 01, 8345 },	{ 16, 31464 },
+				{ 02, 24114 },	{ 17, 31544 },
+				{ 03, 27776 },	{ 18, 25686 },
+				{ 04, 31255 },	{ 19, 27925 },
+				{ 05, 24396 },	{ 20, 30834 },
+				{ 06, 28988 },	{ 21, 23041 },
+				{ 07, 28396 },	{ 22, 28034 },
+				{ 09, 31334 },
+				{ 11, 28372 },
 			}
 		},
 		{	--Head Slot
 			name = "Head",
 			[NORMAL_DIFF] = {
-
+				{1, 8345, nil, nil, "Still BIS"}
 			}
 		},
 		{	--Shoulders
 			name = "Shoulders",
 			[NORMAL_DIFF] = {
-
+				{1,25790, nil, nil, "Easy to Acquire, Alliance only" },
+				{2,27434, nil, nil, "Horde Alternative" },
+				{3,27776, nil, nil, "Horde Alternative" },
+				{4,27797, nil, nil, "BiS" },
+				{5,27995, nil, nil, "Pre-raid Alternative" },
 			},
 		},
 		{	--Back
 			name = "Back",
 			[NORMAL_DIFF] = {
-
+				{1,31255, nil, nil, "BiS" },
+				{2,27878, nil, nil, "Alternative" },
+				{3,28031, nil, nil, "Alliance Alternative" },
+				{4,28032, nil, nil, "Alliance Alternative" },
+				{5,28249, nil, nil, "Alternative" },
 			},
 		},
 		{	--Chest
 			name = "Chest",
 			[NORMAL_DIFF] = {
-
+				{1,28204, nil, nil, "Easy to Acquire" },
+				{2,24396, nil, nil, "Easy to Acquire, Over Hit" },
+				{3,29525, nil, nil, "BiS, Very expensive" },
+				{4,28264, nil, nil, "Alternative" },
+				{5,27461, nil, nil, "Alternative" },
 			},
 		},
 		{	--Wrist
 			name = "Wrist",
 			[NORMAL_DIFF] = {
-
+				{1,28988, nil, nil, "Easy to acquire" },
+				{2,28424, nil, nil, "Easy to acquire" },
+				{3,28171, nil, nil, "Over Hit" },
+				{4,29246, nil, nil, "BiS" },
+				{5,29527, nil, nil, "Alternative" },
+				{6,21602, nil, nil, "Alternative" },
 			},
 
 		},
 		{	--Hands
 			name = "Hands",
 			[NORMAL_DIFF] = {
-
+				{1,28396, nil, nil, "Easy to acquire" },
+				{2,25685, nil, nil, "Over Hit" },
+				{3,27509, nil, nil, "Over Hit" },
+				{4,29507, nil, nil, "BiS" },
+				{5,27531, nil, nil, "Alternative" },
 			},
 		},
 		{	--Waist
 			name = "Waist",
 			[NORMAL_DIFF] = {
-
+				{1,30372, nil, nil, "Easy to Acquire, Scryer only" },
+				{2,31464, nil, nil, "Aldor Alternative" },
+				{3,29247, nil, nil, "BiS" },
+				{4,27760, nil, nil, "Alternative" },
+				{5,27911, nil, nil, "Alternative" },
 			},
 		},
 		{	--Legs
 			name = "Legs",
 			[NORMAL_DIFF] = {
-
+				{1,31544, nil, nil, "BiS" },
+				{2,27837, nil, nil, "Alternative" },
+				{3,25687, nil, nil, "Alternative" },
+				{4,30538, nil, nil, "Alternative" },
+				{5,30535, nil, nil, "Alternative" },
 			},
 		},
 		{	--Feet
 			name = "Feet",
 			[NORMAL_DIFF] = {
-
+				{1,25686, nil, nil, "BiS" },
+				{2,31288, nil, nil, "Alternative" },
+				{3,29248, nil, nil, "Alternative" },
+				{4,30939, nil, nil, "Alternative" },
+				{5,28987, nil, nil, "Alternative" },
+				{6,28422, nil, nil, "Alternative" },
 			},
 		},
 		{	--Neck
 			name = "Neck",
 			[NORMAL_DIFF] = {
-
+				{1,24114, nil, nil, "BiS" },
+				{2,31275, nil, nil, "Pre-raid Budget Option" },
+				{3,29381, nil, nil, "Best Budget Option" },
+				{4,29119, nil, nil, "Alternative" },
+				{5,27779, nil, nil, "Alternative" },
+				{6,19377, nil, nil, "Pre-raid Alternative" },
+				{7,25562, nil, nil, "Pre-raid Alternative" },
 			},
 		},
 		{	--Rings
 			name = "Rings",
 			[NORMAL_DIFF] = {
-
+				{1,30365, nil, nil, "BiS, Aldor only" },
+				{2,30834, nil, nil, "BiS" },
+				{3,27925, nil, nil, "Scryer Alternative" },
+				{4,30860, nil, nil, "Close Second, Aldor only" },
+				{5,31077, nil, nil, "Scryer Alternative" },				
 			},
 		},
-		{	--Trinkets 1
-			name = "Trinkets1",
+		{	--Trinkets
+			name = "Trinkets",
 			[NORMAL_DIFF] = {
-
+				{1,23207, nil, nil, "BiS" },
+				{2,29383, nil, nil, "BiS" },
+				{3,23041, nil, nil, "BiS" },
+				{4,28034, nil, nil, "Close Second" },
+				{5,28121, nil, nil, "Close Second" },
+				{6,19406, nil, nil, "Close Second" },
+				{7,29776, nil, nil, "Alliance Alternative" },
+				{8,28288, nil, nil, "Alternative" },
 			},
 		},
 		{	--Trinkets 2
-			name = "Trinkets2",
+			name = "Weapons",
 			[NORMAL_DIFF] = {
-
+				{1,31334, nil, nil, "Easy to acquire" },
+				{2,28476, nil, nil, "BiS" },
+				{3,29359, nil, nil, "Alternative" },
+				{4,29171, nil, nil, "Alternative" },
+				{5,27877, nil, nil, "Alternative" },
+				{6,28325, nil, nil, "Pre-raid Alternative" },
+				{7,30010, nil, nil, "Pre-raid Alternative" },
+				{8,28948, nil, nil, "Pre-raid Alternative" },
+				{9,28919, nil, nil, "Pre-raid Alternative" },
 			},
 		},
 		{	--Trinkets 3
-			name = "Trinkets3",
+			name = "Idols",
 			[NORMAL_DIFF] = {
-
-			},
-		},
-		{	--1H Wep
-			name = "1-Handed Weapons",
-			[NORMAL_DIFF] = {
-
-			},
-		},
-		{	--2H Wep
-			name = "2-Handed Weapons",
-			[NORMAL_DIFF] = {
-
-			},
-		},
-		{	--Ranged
-			name = "Ranged",
-			[NORMAL_DIFF] = {
-
-			},
-		},
-		{	--Offhand
-			name = "OffHand",
-			[NORMAL_DIFF] = {
-
+				{1,28372, nil, nil, "Recommended Pre-raid" },
+				{2,29390, nil, nil, "BiS" },
+				{3,28064, nil, nil, "Pre-raid Alternative" },
 			},
 		},
 		el_Gems,
