cfa0b16257213549677cbb56175e4a72d0b59819
OrionShock <Orionshock@gmail.com>
2021-09-06 14:46:32 -0700

    Add/Update Warlock PreBiS

--- AtlasLootClassic_P1_PreBIS_Guide/PreBIS_DPS_Warlock_Affliction.lua
+++ AtlasLootClassic_P1_PreBIS_Guide/PreBIS_DPS_Warlock_Affliction.lua
@@ -85,106 +85,178 @@ data[ _FilePrefix ] ={
 	gameVersion = 2,
 	items = {
 		{
-			name = "Set1",
+			name = "PreBIS Example (w/ 3% Hit totem)",
 			[NORMAL_DIFF] = {
-
+				{01,28193}, { 16,30038},
+				{02,28134}, { 17,24262},
+				{03,21869}, { 18,21870},
+				{04,23050}, { 19,28227},
+				{05,21871}, { 20,21709},
+				{06,24250}, { 21,29370},
+				{07,27465}, { 22,27683},
+				{09,29155},
+				{10,29273},
+				{11,22821},
 			}
 		},
 		{	--Head Slot
 			name = "Head",
 			[NORMAL_DIFF] = {
-
+				{1,32494, nil, nil, "Best" },
+				{2,24266, nil, nil, "BiS" },
+				{3,31104, nil, nil, "Alternative Over Hit" },
+				{4,28278, nil, nil, "Alternative Over Hit" },
+				{5,28415, nil, nil, "Alternative Over Hit" },
+				{6,28169, nil, nil, "Horde Only", [ATLASLOOT_IT_ALLIANCE] = false, [ATLASLOOT_IT_HORDE] = true },
 			}
 		},
 		{	--Shoulders
 			name = "Shoulders",
 			[NORMAL_DIFF] = {
-
+				{1,21869, nil, nil, "BiS" },
+				{2,22507, nil, nil, "Second Best" },
+				{3,27796, nil, nil, "Best Under Hit Cap" },
+				{4,27994, nil, nil, "Alternative Under Hit Cap" },
+				{5,30925, nil, nil, "Best Over Hit Cap" },
 			},
 		},
 		{	--Back
 			name = "Back",
 			[NORMAL_DIFF] = {
-
+				{1,23050, nil, nil, "BiS" },
+				{2,27981, nil, nil, "Alternative" },
+				{3,22731, nil, nil, "Alternative" },
+				{4,31140, nil, nil, "Easy to Obtain" },
 			},
 		},
 		{	--Chest
 			name = "Chest",
 			[NORMAL_DIFF] = {
-
+				{1,21871, nil, nil, "BiS Over Hit Cap" },
+				{2,31297, nil, nil, "BiS Under Hit Cap" },
+				{3,22504, nil, nil, "Alternative" },
+				{4,29341, nil, nil, "Alternative" },
+				{5,28191, nil, nil, "Alternative" },
+				{6,28232, nil, nil, "Alternative" },
 			},
 		},
 		{	--Wrist
 			name = "Wrist",
 			[NORMAL_DIFF] = {
-
+				{1,21186, nil, nil, "BiS" },
+				{2,24250, nil, nil, "BiS" },
+				{3,27462, nil, nil, "Optional" },
 			},
 
 		},
 		{	--Hands
 			name = "Hands",
 			[NORMAL_DIFF] = {
-
+				{1,31149, nil, nil, "BiS" },
+				{2,21585, nil, nil, "Classic Option" },
+				{3,27465, nil, nil, "Under Hit Cap" },
+				{4,27537, nil, nil, "Under Hit Cap" },
+				{5,24450, nil, nil, "Under Hit Cap" },
 			},
 		},
 		{	--Waist
 			name = "Waist",
 			[NORMAL_DIFF] = {
-
+				{1,30038, nil, nil, "Best" },
+				{2,24256, nil, nil, "BiS Over Hit Option" },
+				{3,29241, nil, nil, "BiS Under Hit" },
+				{4,22730, nil, nil, "Alternative" },
+				{5,31461, nil, nil, "Over Hit Cap" },
+				{6,27795, nil, nil, "Under Hit Cap" },
 			},
 		},
 		{	--Legs
 			name = "Legs",
 			[NORMAL_DIFF] = {
-
+				{1,24262, nil, nil, "BiS" },
+				{2,30531, nil, nil, "BiS over Hit Cap" },
+				{3,23070, nil, nil, "Over Hit Cap" },
+				{4,19133, nil, nil, "Over Hit Cap" },
+				{5,27948, nil, nil, "Under Hit Cap" },
+				{6,27907, nil, nil, "Under Hit Cap" },
 			},
 		},
 		{	--Feet
 			name = "Feet",
 			[NORMAL_DIFF] = {
-
+				{1,21870, nil, nil, "BiS" },
+				{2,27821, nil, nil, "Under Hit Cap" },
+				{3,28406, nil, nil, "Over Hit Cap" },
+				{4,28179, nil, nil, "Over Hit Cap" },
+				{5,22508, nil, nil, "Over Hit Cap" },
 			},
 		},
 		{	--Neck
 			name = "Neck",
 			[NORMAL_DIFF] = {
-
+				{1,28134, nil, nil, "BiS" },
+				{2,27758, nil, nil, "BiS" },
+				{3,21608, nil, nil, "Optional" },
+				{4,23057, nil, nil, "Optional" },
+				{5,18814, nil, nil, "Optional" },
 			},
 		},
 		{	--Rings
 			name = "Rings",
 			[NORMAL_DIFF] = {
-
+				{1,29172, nil, nil, "BiS Under Hit Cap" },
+				{2,28227, nil, nil, "BiS Under Hit Cap" },
+				{3,28555, nil, nil, "Alternative Under Hit" },
+				{4,29126, nil, nil, "BiS Over Hit Cap" },
+				{5,21709, nil, nil, "Alternative" },
+				{6,23031, nil, nil, "Alternative" },
+				{7,23025, nil, nil, "Alternative" },
 			},
 		},
 		{	--Trinkets
 			name = "Trinkets",
 			[NORMAL_DIFF] = {
-
+				{1,23207, nil, nil, "Demons and Undead" },
+				{2,29370, nil, nil, "BiS" },
+				{3,27683, nil, nil, "BiS" },
+				{4,29132, nil, nil, "Under Hit Cap" },
+				{5,19379, nil, nil, "Under Hit Alternative" },
+				{6,23046, nil, nil, "Alternative" },
+				{7,19337, nil, nil, "Optional" },
 			},
 		},
 		{	--1-Handed Weapons
 			name = "1-Handed Weapons",
 			[NORMAL_DIFF] = {
-
+				{1,31336, nil, nil, "BiS" },
+				{2,30787, nil, nil, "Demon" },
+				{3,nil, nil, nil, "Alternative", [ATLASLOOT_IT_HORDE] = 29155, [ATLASLOOT_IT_ALLIANCE] = 29153 },
+				{4,27905, nil, nil, "Optional" },
 			},
 		},
 		{	--2-Handed Weapons
 			name = "2-Handed Weapons",
 			[NORMAL_DIFF] = {
-
+				{1,22589, nil, nil, "BiS" },
 			},
 		},
 		{	--Wands
 			name = "Wands",
 			[NORMAL_DIFF] = {
-
+				{1,22821, nil, nil, "BiS" },
+				{2,29350, nil, nil, "BiS" },
+				{3,22820, nil, nil, "Alternative" },
+				{4,28386, nil, nil, "Alternative" },
 			},
 		},
 		{	--Offhands
 			name = "Offhands",
 			[NORMAL_DIFF] = {
-
+				{1,29273, nil, nil, "BiS Over Hit Cap" },
+				{2,29272, nil, nil, "Alternative Over Hit Capl" },
+				{3,28412, nil, nil, "BiS Under Hit Cap" },
+				{4,23049, nil, nil, "Alternative Under Hit Cap" },
+				{5,21597, nil, nil, "Alternative Under Hit Cap" },
 			},
 		},
 		el_Gems,
--- AtlasLootClassic_P1_PreBIS_Guide/PreBIS_DPS_Warlock_Destruction.lua
+++ AtlasLootClassic_P1_PreBIS_Guide/PreBIS_DPS_Warlock_Destruction.lua
@@ -84,106 +84,197 @@ data[ _FilePrefix ] ={
 	gameVersion = 2,
 	items = {
 		{
-			name = "Set1",
+			name = "Shadow Destruction (w/ 3% Hit Totem)",
 			[NORMAL_DIFF] = {
-
+				{01,28193}, { 16,30038},
+				{02,28134}, { 17,24262},
+				{03,21869}, { 18,21870},
+				{04,23050}, { 19,28227},
+				{05,21871}, { 20,21709},
+				{06,24250}, { 21,29370},
+				{07,27465}, { 22,27683},
+				{09,29155},
+				{10,29273},
+				{11,22821},
+			}
+		},
+		{
+			name = "Fire Destruction (w/ 3% Hit Totem)",
+			[NORMAL_DIFF] = {
+				{01,28193}, { 16,21846},
+				{02,28134}, { 17,24262},
+				{03,27796}, { 18,28406},
+				{04,23050}, { 19,28227},
+				{05,21848}, { 20,21709},
+				{06,24250}, { 21,29370},
+				{07,21847}, { 22,27683},
+				{09,29155},
+				{10,29270},
+				{11,22821},3
 			}
 		},
 		{	--Head Slot
 			name = "Head",
 			[NORMAL_DIFF] = {
-
+				{1,32494, nil, nil, "Best" },
+				{2,24266, nil, nil, "Best" },
+				{3,31104, nil, nil, "Alternative Over Hit" },
+				{4,28278, nil, nil, "Alternative Over Hit" },
+				{5,28415, nil, nil, "Alternative Over Hit" },
+				{6,28169, nil, nil, "Horde Only", [ATLASLOOT_IT_ALLIANCE] = false, [ATLASLOOT_IT_HORDE] = true },
 			}
 		},
 		{	--Shoulders
 			name = "Shoulders",
 			[NORMAL_DIFF] = {
-
+				{1,21869, nil, nil, "BiS" },
+				{2,22507, nil, nil, "Second Best" },
+				{3,27796, nil, nil, "Best Under Hit Cap" },
+				{4,27994, nil, nil, "Alternative Under Hit Cap" },
+				{5,30925, nil, nil, "Best Over Hit Cap" },
 			},
 		},
 		{	--Back
 			name = "Back",
 			[NORMAL_DIFF] = {
-
+				{1,23050, nil, nil, "BiS" },
+				{2,27981, nil, nil, "Alternative" },
+				{3,22731, nil, nil, "Alternative" },
+				{4,31140, nil, nil, "Easy to Obtain" },
 			},
 		},
 		{	--Chest
 			name = "Chest",
 			[NORMAL_DIFF] = {
-
+				{1,21871, nil, nil, "Shadown BiS Over Hit Cap" },
+				{2,21848, nil, nil, "Fire BiS" },
+				{3,31297, nil, nil, "BiS Under Hit Cap" },
+				{4,22504, nil, nil, "Alternative" },
+				{5,29341, nil, nil, "Alternative" },
+				{6,28191, nil, nil, "Alternative" },
+				{7,28232, nil, nil, "Alternative" },
 			},
 		},
 		{	--Wrist
 			name = "Wrist",
 			[NORMAL_DIFF] = {
-
+				{1,21186, nil, nil, "BiS" },
+				{2,24250, nil, nil, "BiS" },
+				{3,27462, nil, nil, "Optional" },
 			},
 
 		},
 		{	--Hands
 			name = "Hands",
 			[NORMAL_DIFF] = {
-
+				{1,21847, nil, nil, "Fire BiS" },
+				{2,31149, nil, nil, "BiS" },
+				{3,21585, nil, nil, "Classic Option" },
+				{4,27465, nil, nil, "Under Hit Cap" },
+				{5,27537, nil, nil, "Under Hit Cap" },
+				{6,24450, nil, nil, "Under Hit Cap" },
 			},
 		},
 		{	--Waist
 			name = "Waist",
 			[NORMAL_DIFF] = {
-
+				{1,30038, nil, nil, "Best" },
+				{2,21846, nil, nil, "Fire BiS" },
+				{3,24256, nil, nil, "BiS" },
+				{4,22730, nil, nil, "BiS Under Hit" },
+				{5,31461, nil, nil, "Over Hit Cap" },
+				{6,27795, nil, nil, "Under Hit Cap" },
 			},
 		},
 		{	--Legs
 			name = "Legs",
 			[NORMAL_DIFF] = {
-
+				{1,24262, nil, nil, "BiS" },
+				{2,30531, nil, nil, "BiS over Hit Cap" },
+				{3,23070, nil, nil, "Over Hit Cap" },
+				{4,19133, nil, nil, "Over Hit Cap" },
+				{5,27948, nil, nil, "Under Hit Cap" },
+				{6,27907, nil, nil, "Under Hit Cap" },
 			},
 		},
 		{	--Feet
 			name = "Feet",
 			[NORMAL_DIFF] = {
-
+				{1,21870, nil, nil, "Shadow BiS" },
+				{2,27821, nil, nil, "Under Hit Cap" },
+				{3,28406, nil, nil, "Over Hit Cap" },
+				{4,28179, nil, nil, "Over Hit Cap" },
+				{5,22508, nil, nil, "Over Hit Cap" },
 			},
 		},
 		{	--Neck
 			name = "Neck",
 			[NORMAL_DIFF] = {
-
+				{1,28134, nil, nil, "BiS" },
+				{2,27758, nil, nil, "BiS" },
+				{3,21608, nil, nil, "Optional" },
+				{4,23057, nil, nil, "Optional" },
+				{5,18814, nil, nil, "Optional" },
 			},
 		},
 		{	--Rings
 			name = "Rings",
 			[NORMAL_DIFF] = {
-
+				{1,29172, nil, nil, "BiS Under Hit Cap" },
+				{2,28227, nil, nil, "BiS Under Hit Cap" },
+				{3,28555, nil, nil, "Alternative Under Hit" },
+				{4,29126, nil, nil, "BiS Over Hit Cap" },
+				{5,23237, nil, nil, "Fire Alternative" },
+				{6,21709, nil, nil, "Alternative" },
+				{7,23031, nil, nil, "Alternative" },
+				{8,23025, nil, nil, "Alternative" },
 			},
 		},
 		{	--Trinkets
 			name = "Trinkets",
 			[NORMAL_DIFF] = {
-
+				{1,23207, nil, nil, "BiS" },
+				{2,29370, nil, nil, "BiS" },
+				{3,27683, nil, nil, "Under Hit Cap" },
+				{4,29132, nil, nil, "Under Hit Alternative" },
+				{5,19379, nil, nil, "Alternative" },
+				{6,23046, nil, nil, "Optional" },
 			},
 		},
 		{	--1-Handed Weapons
 			name = "1-Handed Weapons",
 			[NORMAL_DIFF] = {
-
+				{1,31336, nil, nil, "BiS" },
+				{2,30787, nil, nil, "Demon" },
+				{3,nil, nil, nil, "Alternative", [ATLASLOOT_IT_HORDE] = 29155, [ATLASLOOT_IT_ALLIANCE] = 29153 },
+				{4,27905, nil, nil, "Optional" },
 			},
 		},
 		{	--2-Handed Weapons
 			name = "2-Handed Weapons",
 			[NORMAL_DIFF] = {
-
+				{1,22589, nil, nil, "BiS" },
 			},
 		},
 		{	--Wands
 			name = "Wands",
 			[NORMAL_DIFF] = {
-
+				{1,22128, nil, nil, "BiS" },
+				{1,22821, nil, nil, "BiS" },
+				{2,29350, nil, nil, "Alternative" },
+				{3,22820, nil, nil, "Alternative" },
+				{4,28386, nil, nil, "Alternative" },
 			},
 		},
 		{	--Offhands
 			name = "Offhands",
 			[NORMAL_DIFF] = {
-
+				{1,29270, nil, nil, "Fire BiS" },
+				{2,29273, nil, nil, "BiS Over Hit Cap" },
+				{3,29272, nil, nil, "Alternative Over Hit Cap" },
+				{4,28412, nil, nil, "BiS Under Hit Cap" },
+				{5,23049, nil, nil, "Alternative Under Hit Cap" },
+				{6,21597, nil, nil, "Alternative Under Hit Cap" },
 			},
 		},
 		el_Gems,
