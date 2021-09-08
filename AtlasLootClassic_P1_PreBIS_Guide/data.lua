702613b67ed65a39685bb669802d55e5eed06290
OrionShock <Orionshock@gmail.com>
2021-09-07 17:04:32 -0700

    fix colors & update loader for warlock

--- AtlasLootClassic_P1_PreBIS_Guide/data.lua
+++ AtlasLootClassic_P1_PreBIS_Guide/data.lua
@@ -10,9 +10,9 @@ local NORMAL_DIFF = data:AddDifficulty(AL["Normal"], "n", 1, nil, true)
 
 local NORMAL_ITTYPE = data:AddItemTableType("Item", "Item")
 local PROF_ITTYPE = data:AddItemTableType("Profession", "Item")
-local roleType_Tank = data:AddContentType("Tank", { 0.78, 0.61, 0.43 })
-local roleType_Heal = data:AddContentType("Healer", { 1.00, 1.00, 1.00})
-local roleType_DPS = data:AddContentType("DPS", { 1.00, 0.96, 0.41 })
+local roleType_Tank = data:AddContentType("Tank", { .77, .12, .023, 1 })
+local roleType_Heal = data:AddContentType("Healer", { 0, 1, .59, 1})
+local roleType_DPS = data:AddContentType("DPS", {0.788, 0.259, 0.992, 1})
 
 --stash all the locals in the AddonScope so each class can have it's own file for sanity sake
 addonScope.AL = AL
